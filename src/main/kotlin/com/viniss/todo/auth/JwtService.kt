404c39a86ea89d8f223d56ba73c344a13bd1e8c6
vinicius-ssantos <viniciusolicveira@gmail.com>
2025-10-14 19:49:53 -0300

    feat(auth): JwtService

--- src/main/kotlin/com/viniss/todo/auth/JwtService.kt
+++ src/main/kotlin/com/viniss/todo/auth/JwtService.kt
@@ -11,36 +11,41 @@ import java.util.*
 
 @Component
 class JwtService {
-private val secret: String = System.getenv("APP_JWT_SECRET")
-?: "dev-secret-please-change-dev-secret-please-change-1234567890"
+
+    private val secret: String =
+        System.getenv("APP_JWT_SECRET") ?: "dev-secret-please-change-dev-secret-please-change-1234567890"
 
 
-private val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
-private val ttlSeconds: Long = 3600 // 1h
+    private val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
+    private val ttlSeconds: Long = 3600 // 1h
 
 
-fun generateToken(email: String, userId: UUID): String {
-val now = Instant.now()
-return Jwts.builder()
-.subject(email)
-.claim("uid", userId.toString())
-.issuedAt(Date.from(now))
-.expiration(Date.from(now.plusSeconds(ttlSeconds)))
-.signWith(key)
-.compact()
-}
+    fun generateToken(email: String, userId: UUID): String {
+        val now = Instant.now()
+        return Jwts.builder()
+            .subject(email)
+            .claim("uid", userId.toString())
+            .issuedAt(Date.from(now))
+            .expiration(Date.from(now.plusSeconds(ttlSeconds)))
+            .signWith(key)
+            .compact()
+    }
 
 
-fun extractEmail(token: String): String =
-Jwts.parser().verifyWith(key).build().parseSignedClaims(token).payload.subject
+    fun extractEmail(token: String): String =
+        Jwts.parser().verifyWith(key).build().parseSignedClaims(token).payload.subject
 
 
-fun extractUserId(token: String): UUID =
-UUID.fromString(Jwts.parser().verifyWith(key).build().parseSignedClaims(token).payload["uid", String::class.java])
+    fun extractUserId(token: String): UUID =
+        UUID.fromString(
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token).payload["uid", String::class.java]
+        )
 
 
-fun isValid(token: String): Boolean = try {
-Jwts.parser().verifyWith(key).build().parseSignedClaims(token)
-true
-} catch (ex: Exception) { false }
+    fun isValid(token: String): Boolean = try {
+        Jwts.parser().verifyWith(key).build().parseSignedClaims(token)
+        true
+    } catch (ex: Exception) {
+        false
+    }
 }
