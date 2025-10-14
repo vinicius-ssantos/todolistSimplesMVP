a033acc6aeeb1b3ed09feb2f0aa217421dfde437
vinicius-ssantos <viniciusolicveira@gmail.com>
2025-10-14 19:41:04 -0300

    feat(auth): entidade e repositório de usuário

---
+++ src/main/kotlin/com/viniss/todo/auth/AppUserRepository.kt
@@ -0,0 +1,11 @@
+package com.viniss.todo.auth
+
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+
+interface AppUserRepository : JpaRepository<AppUserEntity, UUID> {
+fun findByEmail(email: String): AppUserEntity?
+fun existsByEmail(email: String): Boolean
+}
